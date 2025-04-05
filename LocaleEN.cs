d70db0545d44f5f32a20a135acebb1235958732e
EH500_Kintarou <EH500_Kintarou@example.com>
2025-04-06 02:02:10 +0900

    設定画面を追加

---
+++ LocaleEN.cs
@@ -0,0 +1,62 @@
+﻿using Colossal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostalHelper;
+
+public class LocaleEN : IDictionarySource
+{
+	private readonly Setting m_Setting;
+	public LocaleEN(Setting setting)
+	{
+		m_Setting = setting;
+	}
+	public IEnumerable<KeyValuePair<string, string>> ReadEntries(IList<IDictionaryEntryError> errors, Dictionary<string, int> indexCounts)
+	{
+		return new Dictionary<string, string>
+		{
+			{ m_Setting.GetSettingsLocaleID(), "Postal Helper" },
+			{ m_Setting.GetOptionTabLocaleID(Setting.MainSection), "Main" },
+			{ m_Setting.GetOptionGroupLocaleID(Setting.PostOfficeGroup), "Post Office" },
+			{ m_Setting.GetOptionGroupLocaleID(Setting.PostSortingFacilityGroup), "Post Sorting Facility" },
+			{ m_Setting.GetOptionGroupLocaleID(Setting.ResetGroup), "Reset" },
+
+			{ m_Setting.GetOptionLabelLocaleID(nameof(Setting.PO_GetLocalMails)), "Get local mails" },
+			{ m_Setting.GetOptionLabelLocaleID(nameof(Setting.PO_TriggerPercentage)), "Threshold of getting" },
+			{ m_Setting.GetOptionLabelLocaleID(nameof(Setting.PO_GettingPercentage)), "Getting amount" },
+			{ m_Setting.GetOptionLabelLocaleID(nameof(Setting.PO_DisposeOverflow)), "Dispose overflow" },
+			{ m_Setting.GetOptionLabelLocaleID(nameof(Setting.PO_OverflowPercentage)), "Overflow amount" },
+
+			{ m_Setting.GetOptionDescLocaleID(nameof(Setting.PO_GetLocalMails)), "Activate getting local mails when remaining is equal or less than the specified percentage of full capacity." },
+			{ m_Setting.GetOptionDescLocaleID(nameof(Setting.PO_TriggerPercentage)), "When the remaining amaount percentage of full capacity is equal or less than this, the spcified amount of unsorted mails will be gotten." },
+			{ m_Setting.GetOptionDescLocaleID(nameof(Setting.PO_GettingPercentage)), "When getting local mails, the amount will be this of the full capacity." },
+			{ m_Setting.GetOptionDescLocaleID(nameof(Setting.PO_DisposeOverflow)), "Activate disposing mails when amount is equal or more than the specified percentage of full capacity." },
+			{ m_Setting.GetOptionDescLocaleID(nameof(Setting.PO_OverflowPercentage)), "When mails is equal or more than this amount of full capacity, cut off to this amount." },
+
+			{ m_Setting.GetOptionLabelLocaleID(nameof(Setting.PSF_GetUnsortedMails)), "Get unsorted mails" },
+			{ m_Setting.GetOptionLabelLocaleID(nameof(Setting.PSF_TriggerPercentage)), "Threshold of getting" },
+			{ m_Setting.GetOptionLabelLocaleID(nameof(Setting.PSF_GettingAmount)), "Getting amount" },
+			{ m_Setting.GetOptionLabelLocaleID(nameof(Setting.PSF_DisposeOverflow)), "Dispose overflow" },
+			{ m_Setting.GetOptionLabelLocaleID(nameof(Setting.PSF_OverflowPercentage)), "Overflow amount" },
+
+			{ m_Setting.GetOptionDescLocaleID(nameof(Setting.PSF_GetUnsortedMails)), "Activate getting unsorted mails when remaining is equal or less than the specified percentage of full capacity." },
+			{ m_Setting.GetOptionDescLocaleID(nameof(Setting.PSF_TriggerPercentage)), "When the remaining amaount percentage of full capacity is equal or less than this, the spcified amount of unsorted mails will be gotten." },
+			{ m_Setting.GetOptionDescLocaleID(nameof(Setting.PSF_GettingAmount)), "When getting unsorted mails, the amount will be this of the full capacity." },
+			{ m_Setting.GetOptionDescLocaleID(nameof(Setting.PSF_DisposeOverflow)), "Activate disposing mails when amount is equal or more than the specified percentage of full capacity." },
+			{ m_Setting.GetOptionDescLocaleID(nameof(Setting.PSF_OverflowPercentage)), "When mails is equal or more than this amount of full capacity, cut off to this amount." },
+
+			{ m_Setting.GetOptionLabelLocaleID(nameof(Setting.ResetToVanilla)), "Reset to Vanilla" },
+			{ m_Setting.GetOptionLabelLocaleID(nameof(Setting.ResetToRecommend)), "Reset to Recommendation" },
+
+			{ m_Setting.GetOptionDescLocaleID(nameof(Setting.ResetToVanilla)), "This configuration is same as vanilla behavior." },
+			{ m_Setting.GetOptionDescLocaleID(nameof(Setting.ResetToRecommend)), "Reset to Recommendation" },
+		};
+	}
+
+	public void Unload()
+	{
+	}
+}
---
+++ Settings.cs
@@ -0,0 +1,153 @@
+﻿using Colossal.IO.AssetDatabase;
+using Game.Modding;
+using Game.Settings;
+using Game.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Entities;
+
+namespace PostalHelper;
+
+[FileLocation(nameof(PostalHelper))]
+[SettingsUIGroupOrder(PostOfficeGroup, PostSortingFacilityGroup, ResetGroup)]
+[SettingsUIShowGroupName(PostOfficeGroup, PostSortingFacilityGroup, ResetGroup)]
+public class Setting : ModSetting
+{
+	public const string MainSection = "Main";
+	public const string PostOfficeGroup = "Post Office";
+	public const string PostSortingFacilityGroup = "Post Sorting Facility";
+	public const string ResetGroup = "Reset";
+
+	public Setting(IMod mod) : base(mod)
+	{
+		if(NotFirstTime) {
+			SetDefaults();
+			NotFirstTime = true;
+		}
+	}
+
+	public override void Apply()
+	{
+		base.Apply();
+		var system = World.DefaultGameObjectInjectionWorld.GetExistingSystemManaged<PostalHelperSystem>();
+		system.Enabled = true;
+	}
+
+	#region Post Office
+
+	[SettingsUISection(MainSection, PostOfficeGroup)]
+	public bool PO_GetLocalMails { get; set; }
+
+	[SettingsUISection(MainSection, PostOfficeGroup)]
+	[SettingsUISlider(min = 0, max = 100, step = 1, scalarMultiplier = 1, unit = Unit.kPercentage)]
+	[SettingsUIDisableByCondition(typeof(Setting), nameof(PO_GetLocalMails), true)]
+	public int PO_TriggerPercentage { get; set; }
+
+	[SettingsUISection(MainSection, PostOfficeGroup)]
+	[SettingsUISlider(min = 0, max = 100, step = 1, scalarMultiplier = 1, unit = Unit.kPercentage)]
+	[SettingsUIDisableByCondition(typeof(Setting), nameof(PO_GetLocalMails), true)]
+	public int PO_GettingPercentage { get; set; }
+
+
+	[SettingsUISection(MainSection, PostOfficeGroup)]
+	public bool PO_DisposeOverflow { get; set; }
+
+	[SettingsUISection(MainSection, PostOfficeGroup)]
+	[SettingsUISlider(min = 0, max = 100, step = 1, scalarMultiplier = 1, unit = Unit.kPercentage)]
+	[SettingsUIDisableByCondition(typeof(Setting), nameof(PO_DisposeOverflow), true)]
+	public int PO_OverflowPercentage { get; set; }
+
+	#endregion
+
+	#region Post Sorting Facility
+
+	[SettingsUISection(MainSection, PostSortingFacilityGroup)]
+	public bool PSF_GetUnsortedMails { get; set; }
+
+	[SettingsUISection(MainSection, PostSortingFacilityGroup)]
+	[SettingsUISlider(min = 0, max = 100, step = 1, scalarMultiplier = 1, unit = Unit.kPercentage)]
+	[SettingsUIDisableByCondition(typeof(Setting), nameof(PSF_GetUnsortedMails), true)]
+	public int PSF_TriggerPercentage { get; set; }
+
+	[SettingsUISection(MainSection, PostSortingFacilityGroup)]
+	[SettingsUISlider(min = 0, max = 100, step = 1, scalarMultiplier = 1, unit = Unit.kPercentage)]
+	[SettingsUIDisableByCondition(typeof(Setting), nameof(PSF_GetUnsortedMails), true)]
+	public int PSF_GettingAmount { get; set; }
+
+
+	[SettingsUISection(MainSection, PostSortingFacilityGroup)]
+	public bool PSF_DisposeOverflow { get; set; }
+
+	[SettingsUISection(MainSection, PostSortingFacilityGroup)]
+	[SettingsUISlider(min = 0, max = 100, step = 1, scalarMultiplier = 1, unit = Unit.kPercentage)]
+	[SettingsUIDisableByCondition(typeof(Setting), nameof(PSF_DisposeOverflow), true)]
+	public int PSF_OverflowPercentage { get; set; }
+
+	#endregion
+
+	#region Reset
+
+	[SettingsUISection(MainSection, ResetGroup)]
+	[SettingsUIButton]
+	public bool ResetToVanilla
+	{
+		set
+		{
+			SetToVanilla();
+			Apply();
+		}
+	}
+
+	[SettingsUISection(MainSection, ResetGroup)]
+	[SettingsUIButton]
+	public bool ResetToRecommend
+	{
+		set
+		{
+			SetDefaults();
+			Apply();
+		}
+	}
+
+	#endregion
+
+	#region Others
+
+	[SettingsUIHidden]
+	public bool NotFirstTime { get; set; }
+
+	#endregion
+
+	public override void SetDefaults()
+	{
+		PO_GetLocalMails = true;
+		PO_TriggerPercentage = 2;
+		PO_GettingPercentage = 20;
+		PO_DisposeOverflow = true;
+		PO_OverflowPercentage = 80;
+
+		PSF_GetUnsortedMails = true;
+		PSF_TriggerPercentage = 2;
+		PSF_GettingAmount = 20;
+		PSF_DisposeOverflow = true;
+		PSF_OverflowPercentage = 80;
+	}
+
+	public void SetToVanilla()
+	{
+		PO_GetLocalMails = false;
+		PO_TriggerPercentage = 2;
+		PO_GettingPercentage = 20;
+		PO_DisposeOverflow = false;
+		PO_OverflowPercentage = 80;
+
+		PSF_GetUnsortedMails = false;
+		PSF_TriggerPercentage = 2;
+		PSF_GettingAmount = 20;
+		PSF_DisposeOverflow = false;
+		PSF_OverflowPercentage = 80;
+	}
+}
